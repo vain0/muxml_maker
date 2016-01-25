f3bb02322b24f0e2c8dece61c7684eb0e7c88405
vain0 <id_vain@yahoo.co.jp>
2016-01-26 06:10:05 +0900

    Define types for converter

---
+++ src/muxml_maker/Converter.fs
@@ -0,0 +1,45 @@
+﻿namespace Muxml
+
+module Converter =
+  type LyricsLine = {
+      // 表示歌詞
+      Show:  string
+      // 入力歌詞
+      Input: string
+  }
+  with
+    static member Empty =
+        { Show = "@"; Input = "@" }
+
+  type TimeTag =
+    | TimeTag of int
+
+  type Interval =
+    | Interval of int
+
+  type TimeTaggedList<'a> =
+      ('a * TimeTag * TimeTag) list
+
+  type IntervalList<'a> =
+      ('a * Interval) list
+
+  type LyricsRepr<'a> =
+    private
+    | WithTimeTag     of TimeTaggedList<'a>
+    | WithInterval    of IntervalList<'a option>
+
+  // 入力歌詞が未設定な歌詞データ
+  type UnreadableLyrics =
+      LyricsRepr<string>
+
+  type Lyrics =
+      LyricsRepr<LyricsLine>
+
+  type MetaData = {
+      Name            : string
+      MusicPath       : string
+      PicsPath        : string option
+      VideoPath       : string option
+      Artist          : string
+      Genre           : string
+  }
