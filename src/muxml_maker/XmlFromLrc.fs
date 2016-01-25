a81d28a08ef4e0b0e394f1bdf21be1587bc70f26
vain0 <id_vain@yahoo.co.jp>
2016-01-26 05:49:40 +0900

    Add xml_from_lrc

--- src/muxml_maker/XmlFromLrc.fs
+++ src/muxml_maker/XmlFromLrc.fs
@@ -1,5 +1,32 @@
 ﻿namespace Muxml
 
 module Converter =
-  let xml_from_lrc lrc =
-    ""
+  module XmlFromLrc =
+    type Info = { Name: string }
+
+    ()
+
+  open XmlFromLrc
+
+  let xml_from_lrc (info: Info) =
+    let lyrics = ""
+
+    let kpm_elem =
+        ""
+        //"<kpm>" + ModelKPM.ToString("f2") + "</kpm>"
+
+    let video_elem =
+        ""
+        //(Video.Uses ? "<video src=\"" + Video.GetFullPath() + "\" scalemode=\"fullwidth\" />\n" : "")
+
+    let pic_elem =
+        ""//(Pic.Uses   ? "<background id=\"" + Pic.GetFullPath() + "\" />\n" : "")
+
+    ( "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>\n"
+    + "<musicXML>\n"
+    + video_elem
+    + pic_elem
+    + "<musicname>" + info.Name + "</musicname>\n"
+    + lyrics
+    + "</musicXML>\n"
+    )
