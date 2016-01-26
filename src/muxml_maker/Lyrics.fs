f56dfb5538746c0d4262fd462def43b6882a3ac0
vain0 <id_vain@yahoo.co.jp>
2016-01-26 22:17:46 +0900

    Define `with_prev_next_tags`

--- src/muxml_maker/Lyrics.fs
+++ src/muxml_maker/Lyrics.fs
@@ -6,18 +6,23 @@ module Lyrics =
   let margin_threshold = 300
   let margin_ratio = (5, 1)
 
+  // タイムタグつきリスト ttl の、各要素に前後のタグの時刻を付加したリスト
+  /// opt: Some or id
+  let with_prev_next_tags opt ttl =
+      assert (ttl |> List.isEmpty |> not)
+      let (_, _, end_r_tag) = ttl |> Seq.last // Use assumption
+      ttl
+      |> List.zipPrevNext
+        (opt (TimeTag 0)) (end_r_tag)
+        (fun (_, _, r_tag) -> r_tag)
+        (fun (_, l_tag, _) -> l_tag)
+      
   /// 省略されたタイムタグを前後のタイムタグで補完する
   let inline try_complete_time_tags (ottl: OptionallyTimeTaggedList<'a>): TimeTaggedList<'a> =
       assert (ottl |> List.isEmpty |> not)
 
       // ottl に、前後のタグの情報を付加したもの
-      let ottlPrevNext =
-          let (_, _, end_r_tag) = ottl |> Seq.last // Use assumption
-          ottl
-          |> List.zipPrevNext
-            (Some (TimeTag 0)) (end_r_tag)
-            (fun (_, _, r_tag) -> r_tag)
-            (fun (_, l_tag, _) -> l_tag)
+      let ottlPrevNext = ottl |> with_prev_next_tags Some
 
       [ for ((line, l_tag_opt, r_tag_opt), prev_r_tag_opt, next_l_tag_opt) in ottlPrevNext do
           let l_tag =
