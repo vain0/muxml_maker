de30af6682c39cefd2bc46f45136210ba5ecdb4c
vain0 <id_vain@yahoo.co.jp>
2016-01-26 19:53:17 +0900

    Define ``Lyrics.to_interval``

--- src/muxml_maker/Lyrics.fs
+++ src/muxml_maker/Lyrics.fs
@@ -2,6 +2,10 @@
 
 [<AutoOpen>]
 module Lyrics =
+  // config
+  let margin_threshold = 300
+  let margin_ratio = (5, 1)
+
   /// 省略されたタイムタグを前後のタイムタグで補完する
   let inline try_complete_time_tags (ottl: OptionallyTimeTaggedList<'a>): TimeTaggedList<'a> =
       assert (ottl |> List.isEmpty |> not)
@@ -38,3 +42,40 @@ module Lyrics =
 
           yield (line, l_tag, r_tag)
         ]
+  
+  let to_interval = function
+    | WithInterval self -> self
+    | WithTimeTag ls ->
+        // ls に、次行の開始時刻を付加したもの
+        let zipped_ls =
+            List.zip ls
+              (match ls |> List.tryLast with
+              | Some (_, _, TimeTag end_time) ->
+                  let l_tags =
+                    ls |> List.tail |> List.map (fun (_, TimeTag time, _) -> time)
+                  List.append l_tags [end_time]
+              | None -> []
+              )
+
+        /// 前行との間の時間差で、今の行の前方に組み込まれる時間
+        let h_margin = ref 0
+
+        [ for (cur_line, next_time) in zipped_ls do
+            let (line, TimeTag l_tag, TimeTag r_tag) = cur_line
+            let l_tag = l_tag + (! h_margin)
+            h_margin := 0
+
+            /// 次行との時間差
+            let margin = next_time - r_tag
+
+            // 行間の時間差を処理
+            if margin >= margin_threshold then
+              yield (Some line, Interval (r_tag - l_tag))
+              yield (None, Interval margin)
+            else
+              // 前後の行に分割して加える
+              let (t, h) = margin_ratio
+              let t_margin   = margin * t / (t + h)
+              h_margin      := margin - t_margin
+              yield (Some line, Interval (r_tag - l_tag + t_margin))
+          ]
