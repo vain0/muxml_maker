5e9f7bfcbd912f3e2ff004f38b015bf26d46936f
vain0 <id_vain@yahoo.co.jp>
2016-01-26 21:31:01 +0900

    Define ``List.trimMean``

--- src/muxml_maker/Util.fs
+++ src/muxml_maker/Util.fs
@@ -20,3 +20,19 @@ module List =
   let rec dropLast = function
     | [] | [_] -> []
     | x :: xs -> x :: dropLast xs
+
+  // 1回の走査で (len, sum, min, max) の4つを計算する
+  let lenSumMinMax add xs =
+      List.fold (fun (len, acc, mi, ma) x ->
+          ( len + 1
+          , (acc, Some x) ||> Option.appendWith add
+          , (mi,  Some x) ||> Option.appendWith min
+          , (ma,  Some x) ||> Option.appendWith max
+          )) (0, None, None, None) xs
+
+  let inline trimMean xs =
+      let (|Or0|) = Option.getOr (LanguagePrimitives.GenericZero)
+      let (len, Or0 sum, Or0 mi, Or0 ma) = xs |> lenSumMinMax (+)
+      let sum =
+          if len <= 2 then sum else sum - mi - ma
+      (sum, len) ||> LanguagePrimitives.DivideByInt
