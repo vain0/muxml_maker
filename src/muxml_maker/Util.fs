4db77f64fd4db5588f2d464c9ad5f48441ff824d
vain0 <id_vain@yahoo.co.jp>
2016-01-26 21:30:41 +0900

    Define ``Option.appendWith``

--- src/muxml_maker/Util.fs
+++ src/muxml_maker/Util.fs
@@ -1,6 +1,16 @@
 ﻿[<AutoOpen>]
 module Util
 
+open Basis.Core
+
+module Option =
+  let appendWith f l r =
+      match (l, r) with
+      | (Some l, Some r) -> Some (f l r)
+      | ((Some _ as x), None)
+      | (None, (Some _ as x)) -> x
+      | (None, None) -> None
+
 module List =
   let rec tryLast = function
     | [] -> None
