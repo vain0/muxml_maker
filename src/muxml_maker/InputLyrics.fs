d669f93f0e623bd2ef22c9e274a8266f9de004a7
vain0 <id_vain@yahoo.co.jp>
2016-01-27 18:34:05 +0900

    Output full lyc after `-i`

--- src/muxml_maker/InputLyrics.fs
+++ src/muxml_maker/InputLyrics.fs
@@ -117,7 +117,8 @@ Commands:
               // TODO: メタデータを付加すべき
               let sb = StringBuilder()
               for (line, l_tag, r_tag) in lyr do
-                  sb.AppendLine(line.Show) |> ignore
+                  sb.AppendLine(sprintf "%s%s%s" (string l_tag) line.Show (string r_tag))
+                    .AppendLine(line.Input) |> ignore
               printfn "%s" (string sb)
 
       | Parser.MyFailure msg -> failwith msg
